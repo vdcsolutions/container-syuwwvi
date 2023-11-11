e4726c1328e72a26711172f028108a34ee49c7b5
Your Name <vdc.solutions.wro@gmail.com>
2023-11-11 21:33:29 +0100

    added paylod_data to pydantic schema and validated it

--- schemas/request_schema.py
+++ schemas/request_schema.py
@@ -10,26 +10,74 @@ class Config(BaseModel):
 
 
 class Action(BaseModel):
-    type: Literal['scrape', 'click']
-    xpath: str
+    type: Literal['scrape', 'click', 'extractor', 'paginator']
 
 
-class Job(BaseModel):
-    config: Config
+class Job(Action):
+    config: Optional[Config] = None
     type: Literal['paginator', 'extractor']
     urls: List
     next_page_button_xpath: Optional[str]
     actions: List[Action]
-    nested: Optional[bool]
+    nested: Optional[bool] = False
+
+
+class NestedJob(Job):
+    urls: str
 
 
 class Scrape(Action):
     label: str
+    xpath: str
+
+
+List[Job]
 
 
 class Click(Action):
-    pass
+    xpath: str
 
 
 class Request(BaseModel):
-    data: List[Job]
+    payload: Job
+
+
+payload_data = {"payload": {
+    "config": {
+        "headless": True,
+        "proxy": "filename",
+        "waitTime": 5000,
+        "browser": "your_browser_here"
+    },
+    "type": "paginator",
+    "urls": ['https://www.google.com/'],
+    "next_page_button_xpath": "xpath to it",
+    "actions": [
+        {
+            "type": "scrape",
+            "xpath": "xpath to it",
+            "label": "page urls",
+
+        },
+        {
+            "type": "extractor",
+            "nested": "True",
+            "urls": "label of scraped urls",
+            "actions": [
+                {
+                    "type": "scrape",
+                    "xpath": "xpath to it",
+                    "label": "page urls"
+                }
+
+                ,
+                {"type": "click",
+                 "xpath": "xpath to it"
+                 }
+            ]
+        }
+    ]
+}
+}
+
+request = Request(**payload_data)
